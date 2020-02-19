4c8323d390398cf62e0192ec00d917ac760aa69e
Miles Higbie <miles.higbie@gmail.com>
2020-02-19 02:15:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const wakeDog = function(dogName, dogBreed){
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  return(`Wake ${dogName} the ${dogBreed}`);
+function wakeDog(name, breed) {
+  console.log(`Wake ${name} the ${breed}`);
+  return `Wake ${name} the ${breed}`
 }
 
-const leashDog = () => {
-  
+function leashDog(name, breed) {
+  console.log(`Leash ${name} the ${breed}`);
+  return `Leash ${name} the ${breed}`
+}
+
+function walkToPark(name, breed) {
+  console.log(`Walk to the park with ${name} the ${breed}`);
+  return `Walk to the park with ${name} the ${breed}`
+}
+
+function throwFrisbee(name, breed) {
+  console.log(`Throw the frisbee for ${name} the ${breed}`);
+  return `Throw the frisbee for ${name} the ${breed}`
+}
+
+function walkHome(name, breed) {
+  console.log(`Walk home with ${name} the ${breed}`);
+  return `Walk home with ${name} the ${breed}`
+}
+
+function unleashDog(name, breed) {
+  console.log(`Unleash ${name} the ${breed}`);
+  return `Unleash ${name} the ${breed}`
 }
